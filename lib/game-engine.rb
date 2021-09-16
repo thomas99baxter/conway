3a674a4e808fdca6fcbf28793896fd8b4886191b
Tom Baxter <thomas99baxter@gmail.com>
2021-09-16 14:15:58 +0100

    feat: add grid initialiser that takes inputted width and height and returns a corresponding hash

---
+++ lib/game-engine.rb
@@ -0,0 +1,13 @@
+# initialises grid for conways game of life
+# param x Integer width of grid
+# param y Integer height of grid
+# returns Hash {
+    # width: x
+    # height: y
+# }
+def grid_initialisation(x, y)
+  return {
+    width: x,
+    height: y,
+  }
+end
--- spec/game-engine_spec.rb
+++ spec/game-engine_spec.rb
@@ -1,5 +1,28 @@
-describe "it should pass" do
-  it "when given true" do
-    expect(5 - 2).to eq 3
+require './lib/game-engine'
+
+describe "game engine" do
+  describe "grid initialisation" do
+    it "should return a hash" do
+      expect(grid_initialisation(0,0).is_a? Hash).to eq true
+    end
+
+    it "should return a hash with identical width and height" do
+      expect(grid_initialisation(4,4).is_a? Hash).to eq true
+      expect(grid_initialisation(4,4)).to eq({:width=>4, :height=>4})
+    end
+
+    it "should return a hash with asymmetric width and height" do
+      expect(grid_initialisation(10,3).is_a? Hash).to eq true
+      expect(grid_initialisation(10,3)).to eq({:width=>10, :height=>3})
+    end
   end
 end
+
+# [[],[],[]]
+# grid = {
+#   width: x
+#   height: y
+#   cells: [
+#     [],[],[]
+#   ]
+# }
