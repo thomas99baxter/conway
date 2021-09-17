2b60eb98a81c3e83fae61c6843d6e98354a63107
Sam Thoburn <samuel.j.thoburn@outlook.com>
2021-09-17 13:47:28 +0100

    Started creating grid

--- lib/game-engine.rb
+++ lib/game-engine.rb
@@ -41,9 +41,46 @@ end
 # end
 
 # add columns
-# while i <= grid.width
+# while j <= grid.width
 #  grid.cells.each |row| do
     # row.push([])
-    # i++
+    # j++
 #   end
 # end
+
+i = 1
+j = 1
+grid = {
+  width: 5,
+  height: 5,
+  tick_count: 0,
+  cells: []
+}
+while i <= grid[:width]
+  grid[:cells].push([])
+  i += 1
+end
+while j <= grid[:cells].length
+  grid[:cells].each do |col|
+    # figure out how to push in correct amount of columns according to grid.height
+    col.push([], [])
+    j += 1
+  end
+end
+
+# create columns
+# while i <= grid[:width]
+#   grid[:cells].push([])
+#   i += 1
+# end
+# while j <= grid[:height]
+#   grid[:cells[j]] << []
+#   j+=1
+# end
+
+
+# 3x3 grid
+#         col1      col2     col3           col1     col2    col3           col1    col2     col3
+# [ [row1[cell1], [cell2], [cell3]], [row2[cell1], [cell2], [cell3]], [row3[cell1], [cell2], [cell3]] ]
+
+puts grid[:cells].to_s
